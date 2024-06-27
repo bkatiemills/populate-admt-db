a156bcee05289b92f5349fc701abc61bb43a9e62
bkatiemills <you@example.com>
2024-06-27 17:00:43 -0400

    minor zhuzhing

--- load_all.sh
+++ load_all.sh
@@ -3,24 +3,25 @@
 # - the argo and argoMeta collections have been created with appropriate indexes and are empty
 # - the rsync results have been mounted at /bulk/ifremer
 
-find /bulk/ifremer/aoml -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > aoml.sh
+find /bulk/ifremer/aoml -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > aoml.sh
 bash aoml.sh
-find /bulk/ifremer/bodc -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > bodc.sh
+find /bulk/ifremer/bodc -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > bodc.sh
 bash bodc.sh
-find /bulk/ifremer/coriolis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > coriolis.sh
+find /bulk/ifremer/coriolis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > coriolis.sh
 bash coriolis.sh
-find /bulk/ifremer/csio -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > csio.sh
+find /bulk/ifremer/csio -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > csio.sh
 bash csio.sh
-find /bulk/ifremer/csiro -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > csiro.sh
+find /bulk/ifremer/csiro -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > csiro.sh
 bash csiro.sh
-find /bulk/ifremer/incois -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > incois.sh
+find /bulk/ifremer/incois -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > incois.sh
 bash incois.sh
-find /bulk/ifremer/jma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > jma.sh
+find /bulk/ifremer/jma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > jma.sh
 bash jma.sh
-find /bulk/ifremer/kma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > kma.sh
+find /bulk/ifremer/kma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > kma.sh
 bash kma.sh
-find /bulk/ifremer/kordi -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > kordi.sh
+find /bulk/ifremer/kordi -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > kordi.sh
 bash kordi.sh
-find /bulk/ifremer/meds -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > meds.sh
+find /bulk/ifremer/meds -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > meds.sh
 bash meds.sh
-find /bulk/ifremer/nmdis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/convert_nc |' > nmdis.sh
+find /bulk/ifremer/nmdis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > nmdis.sh
+bash nmdis.sh
