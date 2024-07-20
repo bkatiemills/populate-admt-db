742dd92a6a57ef2a228c50dc5c96ab81c4d6725e
bkatiemills <you@example.com>
2024-07-20 13:36:34 -0400

    checkpoint

--- load_all.sh
+++ load_all.sh
@@ -4,24 +4,24 @@
 # - the rsync results have been mounted at /bulk/ifremer
 
 find /bulk/ifremer/aoml -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > aoml.sh
-bash aoml.sh
+#bash aoml.sh
 find /bulk/ifremer/bodc -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > bodc.sh
-bash bodc.sh
+#bash bodc.sh
 find /bulk/ifremer/coriolis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > coriolis.sh
-bash coriolis.sh
+#bash coriolis.sh
 find /bulk/ifremer/csio -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > csio.sh
-bash csio.sh
+#bash csio.sh
 find /bulk/ifremer/csiro -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > csiro.sh
-bash csiro.sh
+#bash csiro.sh
 find /bulk/ifremer/incois -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > incois.sh
-bash incois.sh
+#bash incois.sh
 find /bulk/ifremer/jma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > jma.sh
-bash jma.sh
+#bash jma.sh
 find /bulk/ifremer/kma -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > kma.sh
-bash kma.sh
+#bash kma.sh
 find /bulk/ifremer/kordi -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > kordi.sh
-bash kordi.sh
+#bash kordi.sh
 find /bulk/ifremer/meds -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > meds.sh
-bash meds.sh
+#bash meds.sh
 find /bulk/ifremer/nmdis -type f | grep '/profiles/' | grep '.nc$' | sed 's|^|target/release/nc2mongo |' > nmdis.sh
-bash nmdis.sh
+#bash nmdis.sh
